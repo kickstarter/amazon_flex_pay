42b6e5795d2e9dd3425d5b82e8ab2767a02b37df
Lance Ivy <lance@cainlevy.net>
2011-03-10 16:26:24 -0800

    some API::Reserve parameters only found in the Marketplace docs

--- lib/amazon_flex_pay/api/reserve.rb
+++ lib/amazon_flex_pay/api/reserve.rb
@@ -2,7 +2,11 @@ module AmazonFlexPay::API #:nodoc:
   class Reserve < Base #:nodoc:
     attribute :caller_description
     attribute :caller_reference # required
+    attribute :charge_fee_to, :enumeration => :charge_fee_to
     attribute :descriptor_policy, :type => :descriptor_policy
+    attribute :marketplace_fixed_fee, :type => :amount
+    attribute :marketplace_variable_fee
+    attribute :recipient_token_id
     attribute :sender_description
     attribute :sender_token_id # required
     attribute :transaction_amount, :type => :amount # required
